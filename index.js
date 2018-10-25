f6281556d8ab486da5a5221e36be57e6b39b9721
Masahito Okubo 'Mozy <mozy.okubo@gmail.com>
2018-10-25 12:47:34 +0900

    fix wait timing

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ const crawler = async () => {
 
   console.log('maybe home');
 
-  await page.waitForNavigation({timeout: 600000, waitUntil: 'networkidle'});
+  await page.waitForNavigation({timeout: 60000, waitUntil: 'networkidle'});
 
   // goto tradehistory
   await page.goto('https://bitflyer.com/ja-jp/ex/TradeHistory');
